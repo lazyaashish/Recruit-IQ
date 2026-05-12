6a8b0cc546649a57e672da3683e836d08aa9d027
Aashish Raj <aashishraj.spds@gmail.com>
2026-05-13 00:11:03 +0530

    Update schemas.py to limit password

--- backend/app/api/schemas.py
+++ backend/app/api/schemas.py
@@ -11,7 +11,7 @@ from pydantic import BaseModel, EmailStr, Field
 
 class UserCreate(BaseModel):
     email: EmailStr
-    password: str = Field(min_length=8)
+    password: str = Field(..., min_length=8, max_length=72)
     full_name: Optional[str] = None
 
 
